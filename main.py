026dc5f5058986479f0118a9ad3dfecd969ae97b
Juli Kuczkowska <julitakuczkowska@gmail.com>
2021-08-25 15:20:22 +0200

    added variables

---
+++ main.py
@@ -0,0 +1,26 @@
+credit = 12000
+
+Styczen1 = 1.592824484
+Luty1 = -0.453509101
+Marzec1 = 2.324671717
+Kwiecień1c = 1.261254407
+Maj1 = 1.782526286
+Czerwiec1 = 2.329384541
+Lipiec1 = 1.502229842
+Sierpnień1 = 1.782526286
+Wrzesień1 = 2.328848994
+Październik1 = 0.616921348
+Listopad1 = 2.352295886
+Grudzień1 = 0.337779545
+Styczeń2 = 1.577035247
+Luty2 = -0.292781443
+Marzec2 = 2.48619659
+Kwiecień2 = 0.267110318
+Maj2 = 1.417952672
+Czerwiec2 = 1.054243267
+Lipiec2 = 1.480520104
+Sierpnień2 = 1.577035247
+Wrzesień2 = -0.07742069
+Październik2 = 1.165733399
+Listopad2 = -0.404186718
+Grudzień2 = 1.499708521
